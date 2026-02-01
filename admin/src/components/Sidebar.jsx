3ef6872c6d6b0d27f5b3a271b90c264c16012914
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-02-01 10:00:08 +0530

    Add Sidebar component with navigation links for adding items, listing items, and viewing orders

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { FaRegListAlt } from "react-icons/fa";
+import { SiTicktick } from "react-icons/si";
+import { useNavigate } from 'react-router-dom';
 
 function Sidebar() {
+
+    let navigate = useNavigate();
+
     return (
-        <div>Sidebar</div>
+        <div className='w-[18%] min-h-[100vh] border-r-[1px] py-[35px] fixed left-0 top-[70px]'>
+            <div className='flex flex-col gap-4 pl-[20%] text-[15px]'>
+
+                <div className='flex items-center justify-center md:justify-start gap-2 border border-gray-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89]' onClick={() => navigate("/add")}>
+                    <IoIosAddCircleOutline className='w-[25px] h-[25px]' />
+                    <p className='hidden md:block'>Add Items</p>
+                </div>
+
+                <div className='flex items-center justify-center md:justify-start gap-3 border border-gray-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89]' onClick={() => navigate("/lists")}>
+                    <FaRegListAlt className='w-[20px] h-[20px]' />
+                    <p className='hidden md:block'>List Items</p>
+                </div>
+
+                <div className='flex items-center justify-center md:justify-start gap-3 border border-gray-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89]' onClick={() => navigate("/orders")}>
+                    <SiTicktick className='w-[20px] h-[20px]' />
+                    <p className='hidden md:block'>View Orders</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 function Home() {
     return (
         <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-white relative'>
             <Navbar />
+            <Sidebar />
         </div>
     )
 }
