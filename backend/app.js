e582943f4e5876a2a3dd2a7301950f601cbf78c7
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-02-08 01:14:46 +0530

    cors error in backend

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(
     })
 );
 
-app.options("*", cors());
+app.options("/*", cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
