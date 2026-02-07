0f4b20e3e5311b0fc97909869cd9c03f4e148d23
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-02-08 00:47:40 +0530

    axios error fixed in admin

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://localhost:5174"],
+        origin: ["https://ai-powered-ecommerce-frontend.onrender.com", "http://localhost:5174"],
         credentials: true,
     })
 );
